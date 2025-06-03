060990bb89f7073a0acedf8006cfa3b28cc01c69
Костянтин Підодвірний <kostya190807@gmail.com>
2025-06-03 15:05:29 +0200

    Готове 5 дз

---
+++ WebProgram/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProgram.Data.Entities.Identity;
+using WebProgram.Areas.Admin.Models.Users;
+
+namespace WebProgram.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class UsersController(UserManager<UserEntity> userManager, 
+    IMapper mapper) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var model = await userManager.Users
+            .ProjectTo<UserItemViewModel>(mapper.ConfigurationProvider)
+            .ToListAsync();
+
+        return View(model);
+    }
+
+}
---
+++ WebProgram/Areas/Admin/Mapper/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WebProgram.Data.Entities.Identity;
+using WebProgram.Areas.Admin.Models.Users;
+
+namespace WebProgram.Areas.Admin.Mapper;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<UserEntity, UserItemViewModel>()
+            .ForMember(x => x.Image, opt => opt.MapFrom(x => x.Image))
+            .ReverseMap();
+
+    }
+}
